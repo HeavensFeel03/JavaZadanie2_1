2c405090f610b4629d8305cf008a1270368ef65b
Bato <bato19941994@bk.ru>
2023-09-13 07:55:37 +0800

    renameVariable

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 13_676;
-        int b = 20;
-        int c = a / b;
+        int summa = 13_676;
+        int miles = 20;
+        int bonus = summa / miles;
 
 
-        System.out.println("Бонусных миль равно " + c);
+        System.out.println("Бонусных миль равно " + bonus);
     }
 }
