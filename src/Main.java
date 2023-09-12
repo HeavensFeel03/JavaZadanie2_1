8a6525e316b035b40f8c087dce1614484f2e2832
Bato <bato19941994@bk.ru>
2023-09-12 09:55:27 +0800

    commit2

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,9 @@ public class Main {
     public static void main(String[] args) {
         int a = 13_676;
         int b = 20;
-        int c = a/b;
+        int c = a / b;
 
 
-
-
-
-
-        System.out.println("Бонусных миль равно "+ c);
+        System.out.println("Бонусных миль равно " + c);
     }
 }
